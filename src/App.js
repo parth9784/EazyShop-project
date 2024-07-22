e95c9b0cc583a319789621007fd4915efadf3347
parth9784 <pdsharma458@gmail.com>
2024-07-22 21:14:57 +0530

    added the login signup functionality..

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Heading from './Heading';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import ProductList from './ProductList';
 import Dataarr from "./Dataarr";
 import Loading from "./Loading";
@@ -8,8 +8,10 @@ import Foooter from "./Foooter"
 import Pagenf from "./pagenf";
 import Details from "./productdetail";
 import Cart from "./cart";
+import Signup from "./Signup";
+import LoginPage from "./Login";
 function App() {
-
+  const location = useLocation();
   const saved_data=localStorage.getItem("cart") || "{}";
   const data_json=JSON.parse(saved_data);
   const[cart,upcart]=useState(data_json);
@@ -33,12 +35,14 @@ function App() {
     //use rgb(209,217,255)
   return (
     <div className="bg-[#F7F5F7] w-screen flex flex-col gap-20">   
-    <Heading c={totalcount}/>
+     {location.pathname !== "/signup"&& location.pathname !== "/login" && <Heading c={totalcount} />}
         <div className="flex flex-col grow">
         <Routes>
         <Route path="/" element={<ProductList />}></Route>
         <Route path="/ProductDetails/:sku" element={<Details  onaddtocart={handleaddtocart}/>}></Route>
         <Route path="/cart" element={<Cart cart={cart} upcart={upcart}/>}></Route>
+        <Route path="/signup" element={<Signup/>} ></Route>
+        <Route path="/login" element={<LoginPage/>} ></Route>
         <Route path="*" element={<Pagenf/>} ></Route>
         </Routes>
         </div>
